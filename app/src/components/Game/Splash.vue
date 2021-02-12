e6ea673795a8f49baf8d21c09fca060c728b3e63
Janssen Kuhn <noconsulate@gmail.com>
2021-02-12 13:19:03 -0700

    austin's changes merged

--- app/src/components/Game/Splash.vue
+++ app/src/components/Game/Splash.vue
@@ -1,17 +1,15 @@
 <template>
-<div class="h-24 ...">
-  <div class="space-y-3">
-    <div class="flex justify-center bg-gray-200">
-       <div>
-      Welcome to Golf Bets
-    </div>
-    </div>
-        <div class="flex justify-center">
-    <div>
-      <button @click="goForward" class="btn">Continue</button>
-    </div>
+  <div class="h-24 ...">
+    <div class="space-y-3">
+      <div class="flex justify-center bg-gray-200">
+        <div>
+          Welcome to Golf Bets
         </div>
-  </div>
+      </div>
+      <div class="flex justify-center">
+        <button @click="goForward" class="btn">Continue</button>
+      </div>
+    </div>
   </div>
 </template>
 
