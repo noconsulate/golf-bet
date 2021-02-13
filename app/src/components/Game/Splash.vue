106f7ae94799ecc66e2f59944b9f9d4b37628219
Janssen Kuhn <noconsulate@gmail.com>
2021-02-13 10:57:20 -0700

    cleaned up a few ems

--- app/src/components/Game/Splash.vue
+++ app/src/components/Game/Splash.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="h-24 ...">
-    <div class="space-y-3">
-      <div class="flex justify-center bg-gray-200">
-        <div>
-          Welcome to Golf Bets
-        </div>
-      </div>
-      <div class="flex justify-center">
-        <button @click="goForward" class="btn">Continue</button>
-      </div>
+  <div class="space-y-3">
+    <div class="flex justify-center bg-gray-200">
+      <p>
+        Welcome to Golf Bets
+      </p>
+    </div>
+    <div class="flex justify-center">
+      <button @click="goForward" class="btn">Continue</button>
+    </div>
+    <div class="flex justify-center">
       <img src="../../assets/ACTION-logo.png" />
     </div>
   </div>
