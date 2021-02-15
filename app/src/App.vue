e4f11226c3201a64ddca9ed3cec9372790596bb2
Austin McBride <austin.mcbride@gmail.com>
2021-02-14 23:03:13 -0700

    Green

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
     <div
-      class="container mx-auto border flex justify-center bg-blue-200 md:w-1/2 lg:w-1/3"
+      class="container mx-auto border flex justify-center bg-green-500 md:w-1/2 lg:w-1/3"
     >
-      <router-view class="w-3/4 h-screen bg-green-200 pt-8" />
+      <router-view class="w-3/4 h-screen bg-coolGray-200 pt-8" />
     </div>
   </div>
 </template>
