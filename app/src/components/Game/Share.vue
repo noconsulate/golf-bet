d1daffe17c3e1eb51b41ad509b93602ba7643963
Janssen Kuhn <noconsulate@gmail.com>
2021-02-16 11:55:22 -0700

    ios stretching fixed

--- app/src/components/Game/Share.vue
+++ app/src/components/Game/Share.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="space-y-3">
-        <div class="flex justify-center">
-    <p>SHARE GAME</p>
-        </div>
-            <div class="flex justify-center">
-    <img class="block w-48" src="../../assets/qr-code.png" />
-            </div>
-                <div class="flex justify-center">
-    <button class="btn block" @click="goForward">Continue</button>
-                </div>
-              <div class="flex justify-center">
-    <button class="btn" @click="back">Back</button>
-              </div>
-            </div>
+    <div class="flex justify-center">
+      <p>SHARE GAME</p>
+    </div>
+    <div class="flex justify-center">
+      <img class="block w-48 h-full" src="../../assets/qr-code.png" />
+    </div>
+    <div class="flex justify-center">
+      <button class="btn block" @click="goForward">Continue</button>
+    </div>
+    <div class="flex justify-center">
+      <button class="btn" @click="back">Back</button>
+    </div>
+  </div>
 </template>
 
 <script>
--- app/src/components/Game/Splash.vue
+++ app/src/components/Game/Splash.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="space-y-3">
     <div class="flex justify-center">
-      <img src="../../assets/ACTION-logo-white(1).png" />
+      <img src="../../assets/ACTION-logo-white(1).png" class="w-full h-full" />
     </div>
-     <div class="flex justify-center">
+    <div class="flex justify-center">
       <button @click="goForward" class="btn">GET STARTED</button>
     </div>
   </div>
