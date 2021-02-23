34f163167337be6209bbcb98f8427aea0509eb23
Janssen Kuhn <noconsulate@gmail.com>
2021-02-23 12:40:27 -0700

    nice and centered

--- app/src/components/Game/Gameplay.vue
+++ app/src/components/Game/Gameplay.vue
@@ -1,14 +1,12 @@
 <template>
   <div>
-    <!-- @AUSTIN "v-for" is a loop, in this case referencing the value "holes" found in the script under "data". -->
-    <div
-      v-for="index in holes"
-      :key="index"
-      class="space-y-3 flex items-center flex-col"
-    >
+    <!-- @AUSTIN "v-for" is a loop, in this case referencing the value "holes" found in the script under "data". 
+    <template> is just a VUE tag that doesn't get rendered, so we don't have an empty div or other element-->
+    <template v-for="index in holes">
       <!-- @AUSTIN the v-for above would normally repeat the same <div> a bunch of times with a few variances. So the following div has a "v-if" which means it will render only if its unique "index" == currentHole. currentHole gets incremented each timme "next hole" is clicked upon. -->
       <div
         v-if="index == currentHole"
+        :key="index"
         class="space-y-3 flex flex-col items-center"
       >
         <p>Hole {{ currentHole }}</p>
@@ -21,8 +19,8 @@
           <button @click="next" class="btn">Next Hole</button>
         </div>
       </div>
-    </div>
-    <div v-if="currentHole == holes">
+    </template>
+    <div v-if="currentHole == holes" class="flex justify-center pt-3">
       <button @click="goForward" class="btn">Continue</button>
     </div>
   </div>
