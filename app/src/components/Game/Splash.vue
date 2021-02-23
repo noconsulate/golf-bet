8c685aed4811811d56313e1f1c2b843aabe623f8
Janssen Kuhn <noconsulate@gmail.com>
2021-02-23 12:12:17 -0700

    refactor layout

--- app/src/components/Game/Splash.vue
+++ app/src/components/Game/Splash.vue
@@ -1,11 +1,7 @@
 <template>
-  <div class="space-y-3">
-    <div class="flex justify-center">
-      <img src="../../assets/ACTION-logo-white(1).png" class="w-full h-full" />
-    </div>
-    <div class="flex justify-center">
-      <button @click="goForward" class="btn">GET STARTED</button>
-    </div>
+  <div class="flex flex-col items-center">
+    <img src="../../assets/ACTION-logo-white(1).png" class="w-full h-full" />
+    <button @click="goForward" class="btn ">GET STARTED</button>
   </div>
 </template>
 
