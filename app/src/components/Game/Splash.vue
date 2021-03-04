dc11e2d361376b47fb031dc02f326d96c3f958d1
Janssen Kuhn <noconsulate@gmail.com>
2021-03-04 11:29:17 -0700

    image stretch

--- app/src/components/Game/Splash.vue
+++ app/src/components/Game/Splash.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="flex flex-col items-center">
-    <img src="../../assets/ACTION-logo-white(1).png" class="w-full h-full" />
+    <div>
+      <img src="../../assets/ACTION-logo-white(1).png" class="w-full h-full" />
+    </div>
     <button @click="goForward" class="btn ">GET STARTED</button>
   </div>
 </template>
