3f070202c284d585849643f1b14265861d9fd32e
Janssen Kuhn <noconsulate@gmail.com>
2021-03-05 11:29:16 -0700

    copy to clipboard

--- app/src/components/Game/Share.vue
+++ app/src/components/Game/Share.vue
@@ -2,6 +2,7 @@
   <div class="space-y-3 flex items-center flex-col">
     <p>SHARE GAME</p>
     <p class="text-center">{{ link2Share }}</p>
+    <button class="btn" @click="copy2Clipboard">Copy to clipboard</button>
     <p>Waiting for Player 2 to join the game</p>
     <button class="btn" @click="back">Back</button>
   </div>
@@ -28,6 +29,9 @@ export default {
   methods: {
     back() {
       this.$store.dispatch("setSequence", "confirmation");
+    },
+    copy2Clipboard() {
+      navigator.clipboard.writeText(this.link2Share);
     },
   },
   async created() {
