da3539ee92b3acecee3764912d6e9809d497289a
Janssen Kuhn <noconsulate@gmail.com>
2021-03-08 13:02:42 -0700

    new page

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import Join from "../views/Join.vue";
 import Scorecard from "../views/Scorecard.vue";
+import Game from "../views/Game.vue";
 
 Vue.use(VueRouter);
 
@@ -21,6 +22,11 @@ const routes = [
     path: "/scorecard",
     name: "scorecard",
     component: Scorecard,
+  },
+  {
+    path: "/game",
+    name: "game",
+    component: Game,
   },
 ];
 
---
+++ app/src/views/Game.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="w-full">
+    <div class="bg-red-200 flex justify-end">
+      <label for="players">Number of Players</label>
+      <input
+        id="players"
+        type="number"
+        v-model="players"
+        class="border w-12 mx-4"
+      />
+    </div>
+    <div class="flex justify-end">
+      <label for="points">Number of points</label>
+      <input
+        id="points"
+        type="number"
+        v-model="points"
+        class="border w-12 mx-4"
+      />
+    </div>
+  </div>
+</template>
