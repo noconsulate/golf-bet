816b9eb43788ec3d809913b15b3e2564d88c33e1
Janssen Kuhn <noconsulate@gmail.com>
2021-03-09 11:12:37 -0700

    conform

--- app/src/components/Controller/ConfirmGame.vue
+++ app/src/components/Controller/ConfirmGame.vue
@@ -3,15 +3,16 @@
     <div class="controller-title">Confirm Game</div>
 
     <div class="controller-panel">
-      <div class="controller-prev" @mext="prev">
+      <div class="controller-prev" @click="prev">
         prev
       </div>
       <div
         class="flex flex-grow bg-yellow-400 cursor-pointer justify-center items-center text-4xl"
+        @click="confirm"
       >
         LET'S GO!!
       </div>
-      <div class="controller-next" @click="next">next</div>
+      <div class="controller-next" @click="confirm">next</div>
     </div>
   </div>
 </template>
@@ -19,5 +20,13 @@
 <script>
 export default {
   name: "confirmGame",
+  methods: {
+    prev() {
+      this.$store.dispatch("setController", "selectHoles");
+    },
+    confirm() {
+      console.log("yay");
+    },
+  },
 };
 </script>
