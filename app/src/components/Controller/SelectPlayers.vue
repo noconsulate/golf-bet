cb35725d28177baea88c5b3a0f7d01d5b0718a48
Janssen Kuhn <noconsulate@gmail.com>
2021-03-08 17:29:53 -0700

    selected buttons

--- app/src/components/Controller/SelectPlayers.vue
+++ app/src/components/Controller/SelectPlayers.vue
@@ -1,22 +1,38 @@
 <template>
   <div class="flex flex-col">
-    <div class="flex justify-center py-4">Number of Players</div>
+    <div class="flex justify-center py-4">Select Number of Players</div>
     <div class="flex border border-blue-600">
       <div class="bg-purple-500 px-3 flex items-center cursor-pointer">
         <button>prev</button>
       </div>
       <div class="grid grid-cols-2 bg-yellow-400 flex-grow">
-        <div class="flex justify-center text-4xl">
-          <button @click="select(1)">1</button>
+        <div
+          class="flex justify-center text-4xl cursor-pointer"
+          :class="[players == 1 ? selected : null]"
+          @click="select(1)"
+        >
+          1
         </div>
-        <div class="flex justify-center text-4xl">
-          <button @click="select(2)">2</button>
+        <div
+          class="flex justify-center text-4xl cursor-pointer"
+          :class="[players == 2 ? selected : null]"
+          @click="select(2)"
+        >
+          2
         </div>
-        <div class="flex justify-center text-4xl">
-          <button @click="select(3)">3</button>
+        <div
+          class="flex justify-center text-4xl cursor-pointer"
+          :class="[players == 3 ? selected : null]"
+          @click="select(3)"
+        >
+          3
         </div>
-        <div class="flex justify-center text-4xl">
-          <button @click="select(4)">4</button>
+        <div
+          class="flex justify-center text-4xl cursor-pointer"
+          :class="[players == 4 ? selected : null]"
+          @click="select(4)"
+        >
+          4
         </div>
       </div>
       <div
@@ -33,6 +49,8 @@
 export default {
   data() {
     return {
+      // classes
+      selected: "border",
       players: null,
     };
   },
