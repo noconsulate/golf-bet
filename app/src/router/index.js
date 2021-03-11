46acb89fc55cdd6cde179456297d3d037f667e2e
Janssen Kuhn <noconsulate@gmail.com>
2021-03-11 10:35:07 -0700

    old files deleted

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
 import Join from "../views/Join.vue";
 import Scorecard from "../views/Scorecard.vue";
 import Game from "../views/Game.vue";
@@ -8,11 +7,6 @@ import Game from "../views/Game.vue";
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
   {
     path: "/join",
     name: "Join",
