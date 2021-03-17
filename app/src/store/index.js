912de5f2f99814ce325e036908bed062f669d2b1
Janssen Kuhn <noconsulate@gmail.com>
2021-03-17 13:31:27 -0600

    reabstracted

--- app/src/store/index.js
+++ app/src/store/index.js
@@ -9,9 +9,9 @@ export default new Vuex.Store({
   state: {
     navOpen: false,
     controller: "selectPlayers",
-    players: "4",
-    points: "",
-    holes: "",
+    players: "1",
+    points: "20",
+    holes: "18",
 
     myScore: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18],
     opponentScore: [
