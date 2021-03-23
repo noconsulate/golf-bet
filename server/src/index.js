7ba57b80e5d21452df6629373b6e0e120cd62d44
Janssen Kuhn <noconsulate@gmail.com>
2021-03-23 12:49:44 -0600

    routes

--- server/src/index.js
+++ server/src/index.js
@@ -2,13 +2,22 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 
+import routes from './routes'
+
 const app = express();
 
 app.use(cors());
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+// Routes
+
+app.use('/games', routes.game)
 
 app.listen(process.env.PORT, () => {
   console.log('Listening on ' + process.env.PORT)
