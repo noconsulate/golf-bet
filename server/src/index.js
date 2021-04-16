9cb54d4eb08ac19aed784ad9ad61df6b30f552d4
Janssen Kuhn <noconsulate@gmail.com>
2021-04-16 09:57:59 -0600

    time for a new server

--- server/src/index.js
+++ server/src/index.js
@@ -1,42 +1,10 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
-const ParseServer = require("parse-server").ParseServer;
-import ParseDashboard from "parse-dashboard";
 
 import routes from "./routes";
 
 const app = express();
-
-// Config ParseServer
-const parseAPI = new ParseServer({
-  databaseURI: "mongodb+srv://user1:user1@cluster0.skdbh.mongodb.net/golfbet",
-  appId: "1234",
-  masterKey: "1234",
-  serverURL: `http://localhost:${process.env.PORT}/parse`,
-  liveQuery: {
-    classNames: ["Match"],
-  },
-});
-
-// Parse dashboard (needs security)
-const options = { allowInsecureHTTP: false };
-
-const dashboard = new ParseDashboard(
-  {
-    apps: [
-      {
-        serverURL: "mongodb+srv://user1:user1@cluster0.skdbh.mongodb.net/test",
-        appId: "1234",
-        masterKey: "1234",
-
-        serverURL: `http://localhost:${process.env.PORT}/parse`,
-        appName: "MyApp",
-      },
-    ],
-  },
-  options
-);
 
 // Middleware
 
@@ -44,18 +12,11 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Serve Parse API
-app.use("/parse", parseAPI);
+app.use("/", routes);
 
-// Serve parse dashboard
-app.use("/dashboard", dashboard);
 // listen
 
 // normal way to listen to server
-// app.listen(process.env.PORT, () => {
-//   console.log("listening on port: " + process.env.PORT);
-// });
-
-let httpServer = require("http").createServer(app);
-httpServer.listen(2000);
-var parseLiveQueryServer = ParseServer.createLiveQueryServer(httpServer);
+app.listen(process.env.PORT, () => {
+  console.log("listening on port: " + process.env.PORT);
+});
