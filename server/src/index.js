a47161b4f1e39b04f00e00035554cef712589e51
Janssen Kuhn <noconsulate@gmail.com>
2021-04-16 11:05:12 -0600

    supabase

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
+import { createClient } from '@supabase/supabase-js';
 
 const app = express();
+const supabase = createClient(process.env.SUPABASE_URI, process.env.SUPABASE_KEY);
 
 app.use(cors());
 
@@ -10,8 +12,16 @@ app.get('/', (req, res) => {
   res.send('Hello from the server')
 });
 
-app.post('/make_winner/:matchId', (req, res) => {
+app.post('/make_winner/:matchId', async (req, res) => {
   const matchId = req.params.matchId;
+  
+  const { data, error } = await supabase
+    .from('match')
+    .select()
+    .match({id: matchId})
+
+  console.log (data, error);
+
   return res.send('make winner resource ' + matchId)
 });
 
