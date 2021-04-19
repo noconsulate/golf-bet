c64e1091f05a415dc412b3a9eeb1c427803a7510
Janssen Kuhn <noconsulate@gmail.com>
2021-04-19 14:10:37 -0600

    ?

--- server/src/routes/supa.js
+++ server/src/routes/supa.js
@@ -32,45 +32,62 @@ router.post("/initgameover", async (req, res) => {
   const cory = "3dcd632a-4766-4d78-ac73-14a11c9886ea";
   const diane = "6afb6271-875c-4a9b-b70f-488344aaaa20";
 
-  let scores = {};
+  let players = {
+    1: {
+      name: "alice",
+      uuid: "545a0a22-086a-49bf-826a-9b36dc2e9b8c",
+      score_id: "",
+    },
+    2: {
+      name: "bob",
+      uuid: "ecefd855-e09d-40d9-8c92-6424e946ef96",
+      score_id: "",
+    },
+    3: {
+      name: "cory",
+      uuid: "3dcd632a-4766-4d78-ac73-14a11c9886ea",
+      score_id: "",
+    },
+    4: {
+      name: "diane",
+      uuid: "6afb6271-875c-4a9b-b70f-488344aaaa20",
+      score_id: "",
+    },
+  };
+
+  console.log(players[1].score_id);
 
   const newMatch = await supabase.rpc("new_match", {
     players: 4,
     points: 69000,
     is_18_holes: true,
     is_classic_scoring: false,
-    creator: alice,
+    creator: players[1].uuid,
   });
-
-  console.log(newMatch);
 
   const matchId = newMatch.data.match_id;
-  scores.alice = newMatch.data.score_id;
+  players[1].score_id = newMatch.data.score_id;
 
-  const bobJoin = await supabase.rpc("join_match", {
-    match_id: matchId,
-    player_id: bob,
-  });
+  console.log(players[1].score_id);
 
-  console.log(bobJoin);
-  scores.bob = bobJoin.data.score_id;
+  let update = await supabase
+    .from("score")
+    .update(scoresGen())
+    .match({ id: players[1].score_id });
 
-  const coryJoin = await supabase.rpc("join_match", {
-    match_id: matchId,
-    player_id: cory,
-  });
-  scores.cory = coryJoin.data.player_id;
+  for (let i = 2; i <= 4; i++) {
+    let result = await supabase.rpc("join_match", {
+      match_id: matchId,
+      player_id: players[i].uuid,
+    });
+    console.log(result);
+    players[i].score_id = result.data.score_id;
 
-  const dianeJoin = await supabase.rpc("join_match", {
-    match_id: matchId,
-    player_id: diane,
-  });
-  scores.diane = dianeJoin.data.player_id;
-
-  let scoresObj = scoresGen();
-  let update = await supabase.from("score").update({ 1: 3 }).match(scoresGen());
-
-  console.log(update);
+    update = await supabase
+      .from("score")
+      .update(scoresGen())
+      .match({ id: players[i].score_id });
+  }
 
   return res.send("congrtats");
 });
