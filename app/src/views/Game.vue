c61bd24a84331f2455657a10afdec2cfcb5e7d6c
Janssen Kuhn <noconsulate@gmail.com>
2021-04-27 11:05:45 -0600

    refactoring for better ux flow control

--- app/src/views/Game.vue
+++ app/src/views/Game.vue
@@ -1,15 +1,38 @@
 <template>
   <div>
-    <Create />
+    <div v-if="showNoUser">
+      Please sign in to continue
+    </div>
+    <Create v-if="showCreate" />
   </div>
 </template>
 
 <script>
 import Create from "../components/Game/Create"
 export default {
-  name: "create",
+  name: "game",
   components: {
     Create
+  },
+  computed: {
+    showCreate() {
+      if (
+        this.$store.state.active_match == undefined &&
+        !this.showNoUser
+        ) {
+        return true
+      } else
+      {
+        return false
+      }
+    },
+    showNoUser() {
+      if (this.$store.state.user == null) {
+        return true;
+      } else {
+        return false;
+      }
+    }
   }
 }
 </script>
