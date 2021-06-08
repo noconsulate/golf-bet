1ced823b8d326cbae955dc6d858219d07935b396
Janssen Kuhn <noconsulate@gmail.com>
2021-06-08 14:52:35 -0600

    idunoo

--- sql/join_match.sql
+++ sql/join_match.sql
@@ -1,92 +1,107 @@
-create or replace function join_match(
-  match_id text, 
-  player_id uuid,
-  out score_id uuid,
-  out players_joined_out int
-)
-
-language plpgsql
-as $$
-declare
-match_rec record;
-my_player_num int;
-score_id_ret uuid;
-scores_var uuid[];
-participants_var uuid[];
-user_balance int8;
-begin
-	-- get info from match
-	select players, points, players_joined, scores, participants, balance
-    from match
-    where id = match_id
-    into match_rec;
-    
+CREATE OR REPLACE FUNCTION join_match (match_id text, player_id uuid, out score_id uuid, out players_joined_out int)
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    match_rec record;
+    my_player_num int;
+    score_id_ret uuid;
+    scores_var uuid[];
+    participants_var uuid[];
+    user_balance int8;
+BEGIN
+    -- get info from match
+    SELECT
+        players,
+        points,
+        players_joined,
+        scores,
+        participants,
+        balance
+    FROM
+        MATCH
+    WHERE
+        id = match_id INTO match_rec;
     -- get user balance
-    select balance
-    from users
-    where id = player_id
-    into user_balance;
-    
-    if user_balance < match_rec.balance then
-    	raise exception 'insufficient user balance';
-    end if;
-    
+    SELECT
+        balance
+    FROM
+        users
+    WHERE
+        id = player_id INTO user_balance;
+    IF user_balance < match_rec.balance THEN
+        RAISE EXCEPTION 'insufficient user balance';
+    END IF;
     -- if there's room in the game, continue
-    
-    if match_rec.players_joined < match_rec.players then 	
-    
-    	-- check, new player is unique
-    	if array[player_id] <@ match_rec.participants then
-        	raise exception 'player already joined';
-        end if;
-        
+    IF match_rec.players_joined < match_rec.players THEN
+        -- check, new player is unique
+        IF ARRAY[player_id] <@ match_rec.participants THEN
+            RAISE EXCEPTION 'player already joined';
+        END IF;
         -- trasfer tokens
-        update users
-        set balance = balance - match_rec.points
-        where id = player_id;
-        
-        update match
-        set balance = balance + match_rec.points
-        where id = match_id;
-    
-    	my_player_num = match_rec.players_joined + 1;
+        UPDATE
+            users
+        SET
+            balance = balance - match_rec.points
+        WHERE
+            id = player_id;
+        UPDATE
+            MATCH
+        SET
+            balance = balance + match_rec.points
+        WHERE
+            id = match_id;
+        my_player_num = match_rec.players_joined + 1;
         players_joined_out = my_player_num;
-        
         -- create new scores row
-        insert into score(match_id, player_num, player_id)
-        values(match_id, my_player_num, player_id)
-        returning id into score_id_ret;
-    
-    	score_id = score_id_ret;
-        
-        update users
-        set active_score = score_id_ret
-        where id = player_id;
-        
-        update users
-        set active_match = match_id
-        where id = player_id;
-        
-        -- update players_joined in match 
-        update match
-        set players_joined = my_player_num
-        where id = match_id;
-        
+        INSERT INTO score (match_id, player_num, player_id)
+            VALUES (match_id, my_player_num, player_id)
+        RETURNING
+            id INTO score_id_ret;
+        score_id = score_id_ret;
+        UPDATE
+            users
+        SET
+            active_score = score_id_ret
+        WHERE
+            id = player_id;
+        UPDATE
+            users
+        SET
+            active_match = match_id
+        WHERE
+            id = player_id;
+        -- update players_joined in match
+        UPDATE
+            MATCH
+        SET
+            players_joined = my_player_num
+        WHERE
+            id = match_id;
+        -- update player_num in users
+        UPDATE
+            users
+        SET
+            player_num = my_player_num
+        WHERE
+            id = player_id;
         --update scores list in match
         scores_var = array_append(match_rec.scores, score_id_ret);
-        update match
-        set scores = scores_var
-        where id = match_id;
-        
+        UPDATE
+            MATCH
+        SET
+            scores = scores_var
+        WHERE
+            id = match_id;
         -- update participants list in match
         participants_var = array_append(match_rec.participants, player_id);
-        update match
-        set participants = participants_var
-        where id = match_id;
-		    
-  	else
-    	score_id = 0;
-    end if;
-    
-end
+        UPDATE
+            MATCH
+        SET
+            participants = participants_var
+        WHERE
+            id = match_id;
+    ELSE
+        score_id = 0;
+    END IF;
+END
 $$
--- sql/new_match.sql
+++ sql/new_match.sql
@@ -1,76 +1,81 @@
-create or replace function new_match(
-  players integer,
-  points integer,
-  is_18_holes boolean,
-  is_classic_scoring boolean,
-  creator uuid,
-  
-  out match_id text,
-  out score_id uuid
-)
+CREATE OR REPLACE FUNCTION new_match (players integer, points integer, is_18_holes boolean, is_classic_scoring boolean, creator uuid, out match_id text, out score_id uuid)
 -- returns integer
-language plpgsql
-as $$
-declare
+LANGUAGE plpgsql
+AS $$
+DECLARE
   match_id_ret text;
   score_id_ret uuid;
   points_to_transfer int8 = points;
   player_balance int8;
- 
-begin
+BEGIN
   -- check player has enough tokens
-  select balance
-  from users
-  where id = creator
-  into player_balance;
-  
-  if player_balance < points_to_transfer then 
-    raise exception 'not enough tokens';
-  end if;
-	
-  insert into match(players, points, is_18_holes, is_classic_scoring, creator, players_joined)
-  values(players, points, is_18_holes, is_classic_scoring, creator, 1)
-  returning id into match_id_ret;
-
+  SELECT
+    balance
+  FROM
+    users
+  WHERE
+    id = creator INTO player_balance;
+  IF player_balance < points_to_transfer THEN
+    RAISE EXCEPTION 'not enough tokens';
+  END IF;
+  INSERT INTO MATCH (players, points, is_18_holes, is_classic_scoring, creator, players_joined)
+    VALUES (players, points, is_18_holes, is_classic_scoring, creator, 1)
+  RETURNING
+    id INTO match_id_ret;
   match_id = match_id_ret;
-  
   -- create score
-  
-  insert into score(match_id, player_num, player_id)
-  values(match_id, 1, creator)
-  returning id into score_id_ret;
-  
+  INSERT INTO score (match_id, player_num, player_id)
+    VALUES (match_id, 1, creator)
+  RETURNING
+    id INTO score_id_ret;
   score_id = score_id_ret;
-  
-  update users
-  set active_score = score_id_ret
-  where id = creator;
-  
+  UPDATE
+    users
+  SET
+    active_score = score_id_ret
+  WHERE
+    id = creator;
   -- insert score row id into match
-  
-  update match
-  set scores = array[score_id_ret]
-  where id = match_id_ret;
-  
-  -- activate match 
- 
-  update users
-  set active_match = match_id_ret
-  where id = creator;
-  
+  UPDATE
+    MATCH
+  SET
+    scores = ARRAY[score_id_ret]
+  WHERE
+    id = match_id_ret;
+  -- activate match
+  UPDATE
+    users
+  SET
+    active_match = match_id_ret
+  WHERE
+    id = creator;
   -- transfer tokens
-  update match
-  set balance = balance + points_to_transfer
-  where id = match_id_ret;
-  
-  
-  update users
-  set balance = balance - points_to_transfer
-  where id = creator;
-  
+  UPDATE
+    MATCH
+  SET
+    balance = balance + points_to_transfer
+  WHERE
+    id = match_id_ret;
+  UPDATE
+    users
+  SET
+    balance = balance - points_to_transfer
+  WHERE
+    id = creator;
   -- set status
-  update match
-  set status = 'waiting'
-  where id = match_id_ret;
+  UPDATE
+    MATCH
+  SET
+    status = 'waiting'
+  WHERE
+    id = match_id_ret;
+  -- set to player 1
+  UPDATE
+    users
+  SET
+    player_num = 1
+  WHERE
+    id = creator;
+END;
+$$;
 
-end; $$;
