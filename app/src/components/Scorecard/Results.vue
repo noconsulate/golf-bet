c919d8bfd577de2639d33069d1b9a54244825c07
Janssen Kuhn <noconsulate@gmail.com>
2021-06-24 10:37:05 -0600

    grid... gah!!!

--- app/src/components/Scorecard/Results.vue
+++ app/src/components/Scorecard/Results.vue
@@ -1,11 +1,23 @@
 <template>
-  <div class="h-screen w-screen flex flex-col">
-    <p>RESULTS</p>
+  <div class="h-screen w-screen flex flex-col bg-blue-200">
+    <div class="grid grid-cols-7 grid-rows-9 pt-8 p-2 h-full">
+      <div class="col-span-2 bg-red-200">**</div>
+      <div class="col-span-3 justify-self-center bg-red-400">
+        <p class="">Confirm Totals</p>
+      </div>
+      <div class="col-span-2 bg-red-200" />
+      <div :class="card">player 1 bla bla bla</div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "Results",
+  data() {
+    return {
+      card: " row-span-2 col-span-full place-self-stretch bg-green-100",
+    };
+  },
 };
 </script>
