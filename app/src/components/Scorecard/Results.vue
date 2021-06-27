b1e0a696b33939bc2fa88e9d49a5eb81feae5234
Janssen Kuhn <noconsulate@gmail.com>
2021-06-27 16:52:44 -0600

    grid lookin nice

--- app/src/components/Scorecard/Results.vue
+++ app/src/components/Scorecard/Results.vue
@@ -6,7 +6,51 @@
           <p>Confirm Totals</p>
         </div>
       </div>
-      <div class="col-span-2 row-span-2 bg-green-200"></div>
+      <div class="col-span-2 row-span-2 bg-green-200 grid grid-cols-5 mx-2">
+        <div class="col-span-2 flex flex-col justify-center bg-blue-300">
+          <p>Player 1</p>
+          <p>Handicap xx</p>
+        </div>
+        <div
+          class="
+            col-span-1
+            flex flex-col
+            justify-center
+            items-center
+            divide-y divide-black
+            bg-yellow-200
+          "
+        >
+          <p>Net</p>
+          <p>xx</p>
+        </div>
+        <div
+          class="
+            col-span-1
+            flex flex-col
+            justify-center
+            items-center
+            divide-y divide-black
+            bg-yellow-200
+          "
+        >
+          <p>Gross</p>
+          <p>xx</p>
+        </div>
+        <div
+          class="
+            col-span-1
+            flex flex-col
+            justify-center
+            items-center
+            divide-y divide-black
+            bg-yellow-200
+          "
+        >
+          <p>Total</p>
+          <p>xx</p>
+        </div>
+      </div>
       <div class="col-span-2 row-span-2 bg-green-200"></div>
       <div class="col-span-2 row-span-2 bg-green-200"></div>
       <div class="col-span-2 row-span-2 bg-green-200"></div>
