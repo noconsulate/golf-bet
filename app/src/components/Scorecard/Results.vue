af2ca6818897fa43ef4f2aafe1f3fc81a67785dd
Janssen Kuhn <noconsulate@gmail.com>
2021-06-27 09:42:13 -0600

    confirm totals box

--- app/src/components/Scorecard/Results.vue
+++ app/src/components/Scorecard/Results.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="h-screen w-screen">
     <div class="grid grid-cols-2 grid-rows-9-static gap-2 h-full bg-pink-200">
-      <div class="col-span-2 row-span-1 bg-red-800">*</div>
-      <div class="col-span-2 row-span-2 bg-green-200"></div>
-      <div class="row-span-2 bg-green-200">
-        <p>hehe</p>
-        <p>NTMA</p>
+      <div class="col-span-2 row-span-1 bg-red-800">
+        <div class="flex justify-center items-center h-full bg-pink-700">
+          <p>Confirm Totals</p>
+        </div>
       </div>
-      <div class="row-span-2 bg-green-200"></div>
-      <div class="row-span-2 bg-green-200"></div>
+      <div class="col-span-2 row-span-2 bg-green-200"></div>
+      <div class="col-span-2 row-span-2 bg-green-200"></div>
+      <div class="col-span-2 row-span-2 bg-green-200"></div>
+      <div class="col-span-2 row-span-2 bg-green-200"></div>
     </div>
   </div>
 </template>
