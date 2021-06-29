8418a3e3cd388d3852fa88fa01fef84c5655b3ef
Janssen Kuhn <noconsulate@gmail.com>
2021-06-29 13:56:40 -0600

    gameover info for LOSER

--- app/src/components/Scorecard/AfterTotals.vue
+++ app/src/components/Scorecard/AfterTotals.vue
@@ -1,23 +1,40 @@
 <template>
   <div class="h-screen">
-    <div
-      v-if="isWinner"
-      class="grid grid-cols-1 grid-rows-9-static h-full bg-pink-200"
-    >
-      <div class="row-span-1 bg-blue-200"></div>
-      <div class="row-span-3 bg-green-300">
+    <div v-if="isWinner" class="grid grid-cols-1 grid-rows-9-static h-full">
+      <div class="row-span-1"></div>
+      <div class="row-span-3">
         <div class="flex flex-col justify-center items-center h-full">
           <p>Contrats</p>
           <p>Player {{ player.player_num }}</p>
           <p>{{ player.handle }}</p>
         </div>
       </div>
-      <div class="row-span-3 bg-yellow-400">
+      <div class="row-span-3">
         <div class="flex flex-col h-full justify-center items-center">
           <p>You won ${{ netProfit }}</p>
           <p>**</p>
           <p v-for="payment in payments" :key="payment.player">
             ${{ payment.payment }} from {{ playerHandle(payment.player) }},
+            Player {{ payment.player }}
+          </p>
+        </div>
+      </div>
+    </div>
+    <div v-else class="grid grid-cols-1 grid-rows-9-static h-full">
+      <div class="row-span-1"></div>
+      <div class="row-span-3">
+        <div class="flex flex-col justify-center items-center h-full">
+          <p>Bummer</p>
+          <p>Player {{ player.player_num }}</p>
+          <p>{{ player.handle }}</p>
+        </div>
+      </div>
+      <div class="row-span-3">
+        <div class="flex flex-col h-full justify-center items-center">
+          <p>You lost ${{ netProfit }}</p>
+          <p>**</p>
+          <p v-for="payment in payments" :key="payment.player">
+            ${{ -1 * payment.payment }} to {{ playerHandle(payment.player) }},
             Player {{ payment.player }}
           </p>
         </div>
